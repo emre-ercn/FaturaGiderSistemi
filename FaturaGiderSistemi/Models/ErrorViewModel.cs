5c074fd02ba3437697d56d8141bd8c0e83c74aba
Emre Ercan <emrercan9@gmail.com>
2026-07-13 10:11:27 +0300

    ilk kurulum

---
+++ FaturaGiderSistemi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FaturaGiderSistemi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
