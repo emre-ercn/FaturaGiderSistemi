6987a689b33ef970dd6216817a5d9df04d2feb9d
Emre Ercan <emrercan9@gmail.com>
2026-07-31 21:28:42 +0300

    Gun 29: Tüm projeye entegre, localStorage hafızalı Dark Mode (Koyu Tema) eklendi ve Bootstrap 5.3'e gecildi.

--- FaturaGiderSistemi/obj/Debug/net8.0/FaturaGiderSistemi.AssemblyInfo.cs
+++ FaturaGiderSistemi/obj/Debug/net8.0/FaturaGiderSistemi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FaturaGiderSistemi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff40fc5671580d663e788443ef17fc50a24894c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e039e4fed531ec71445b03de6ac526834a87142")]
 [assembly: System.Reflection.AssemblyProductAttribute("FaturaGiderSistemi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FaturaGiderSistemi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
